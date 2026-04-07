9d124afbc414a1b0dd3619c26d50a08fd6e67fdc
Katya Ghanoumi <katyaghanoumi35@gmail.com>
2026-04-07 06:08:16 -0700

    Update home page to confirm deployment

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,17 +1,18 @@
-from django . http import HttpResponse
-from django . urls import path
+from django.http import HttpResponse
+from django.urls import path
 
-def home ( request ) :
+def home (request) :
     return HttpResponse ("""
                          <html>
-                         <head> <title> Azure Django Lab </title> </head>
+                         <head><title> Azure Django Lab </title></head>
                          <body>
-                         <h1> Hello from Azure ! </h1>
-                         <p> This Django app is running on Azure App Service. </p>
-                         <p> Deployed automatically via GitHub Actions CI/CD pipeline . </p>
+                            <h1>Hello from Azure! </h1>
+                            <p>This app is running on Azure App Service. </p>
+                            <p> Deployed automatically by GitHub Actions CI/CD pipeline.</p>
+                            <p><strong> Pipeline is working! </strong></p>
                          </body>
                          </html>
-                         """)
+                        """)
 urlpatterns = [
     path ('', home ) ,
     ]
